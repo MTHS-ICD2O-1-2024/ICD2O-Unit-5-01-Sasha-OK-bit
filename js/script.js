c8c94b71204ab451bcb0746527688b249539ba94
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-04-22 16:45:48 +0000

    Added 5-01 JS

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+function checkNumber () {
+
+  const userNumber = parseInt(document.getElementById('user-number').value)
+
+  if (userNumber === randomNumber)  {
+    document.getElementById('result').innerHTML =
+      '<p>You have got it correct with your guess!</p>'
+  }
+
+  if (userNumber !== randomNumber)  {
+    document.getElementById('result').innerHTML =
+      '<p>You have done wrong with your guess!</p>'
+      '<br/> The correct number was: ' + randomNumber + '</p>'
+  }
+}
