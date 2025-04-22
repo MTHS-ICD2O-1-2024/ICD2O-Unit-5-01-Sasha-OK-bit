b2ba75e35a31732c1d99b851c252b08fdf916550
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-04-22 16:49:12 +0000

    Fixed connections

--- js/script.js
+++ js/script.js
@@ -8,15 +8,15 @@ const randomNumber = Math.floor(Math.random() * 6) + 1
 
 function calculaterandomnumber () {
 
-  const userNumber = parseInt(document.getElementById('user-number').value)
+  const userNumber = parseInt(document.getElementById('usernumber').value)
 
   if (userNumber === randomNumber)  {
-    document.getElementById('result').innerHTML =
+    document.getElementById('answer1').innerHTML =
       '<p>You have got it correct with your guess!</p>'
   }
 
   if (userNumber !== randomNumber)  {
-    document.getElementById('result').innerHTML =
+    document.getElementById('answer1').innerHTML =
       '<p>You have done wrong with your guess!</p>'
       '<br/> The correct number was: ' + randomNumber + '</p>'
   }
